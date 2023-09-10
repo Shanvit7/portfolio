248031abb4fec3e25f069fd9ad33f3edef6fc5b7
shanvit <shanvit7@gmail.com>
2023-09-10 16:26:20 +0530

    experince box animation added

--- src/components/DurationSlider.jsx
+++ src/components/DurationSlider.jsx
@@ -1,20 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import anime from "animejs"; // Import Anime.js
 
-const DurationSlider = ({ 
-    setXP = ()=>{} ,
-    month = 'June',
-    year = 2022
+const DurationSlider = ({
+  setXP = () => {},
+  XP = 0,
+  month = "June",
+  year = 2022,
 }) => {
   const [value, setValue] = useState(0);
+
   const handleChange = (event) => {
     setXP(event.target.value);
     setValue(event.target.value);
   };
+
+  const animateText = () => {
+    const textElements = document.querySelectorAll(".month-year-text");
+    const textAnimation = anime({
+      targets: textElements,
+      translateY: [-10, 0],
+      opacity: [0, 1],
+      duration: 500,
+      easing: "easeOutQuad",
+    });
+  };
+
+  useEffect(() => {
+    if (!(XP % 1 !== 0)) {
+      animateText();
+    }
+  }, [XP]);
+
   return (
     <div className="flex flex-col items-center justify-center w-full p-4 lg:p-8">
-        <h5 className="text-center p-8 font-bold text-2xl">
-            {`${month} ${year}`}
-        </h5>
+      <h5 className="text-center p-8 font-bold text-2xl month-year-text">
+        {`${month} ${year}`}
+      </h5>
       <input
         id="rangeSlider"
         type="range"
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -13,10 +13,9 @@ const Work = () => {
     year = null,
   } = getExperienceAccToMonth(Number(XP)) ?? {};
 
-  // Function to animate text elements using Anime.js
   const animateText = () => {
     const textElements = document.querySelectorAll(".animate-text");
-    
+
     anime({
       targets: textElements,
       opacity: [0, 1],
@@ -27,24 +26,37 @@ const Work = () => {
     });
   };
 
+  const animateTitleText = () => {
+    const titleText = document.querySelector(".animate-title-text");
+
+    anime({
+      targets: titleText,
+      opacity: [0.5, 1],
+      translateY: [-25, 0],
+      duration: 800,
+      easing: "easeOutQuad",
+    });
+  };
+
   useEffect(() => {
-    if(!(XP % 1 !== 0)){
+    if (!(XP % 1 !== 0)) {
+      animateTitleText();
       animateText();
     }
   }, [XP]);
 
   return (
     <section className="relative p-8 flex flex-col-reverse lg:flex-col">
-      <h1 className="hidden lg:block text-2xl font-sans font-bold tracking-normal text-inherit antialiased">
+      <h1 className="hidden lg:block text-2xl font-sans font-bold tracking-normal text-inherit antialiased animate-title-text">
         # {projectName}
       </h1>
 
       <div className="flex flex-col lg:flex-row justify-between w-full pt-4 h-full lg:h-72">
         <div className="w-full lg:w-1/2">
-          <h1 className="block lg:hidden text-2xl font-sans font-bold tracking-normal text-inherit antialiased">
+          <h1 className="block lg:hidden text-2xl font-sans font-bold tracking-normal text-inherit antialiased animate-title-text">
             # {projectName}
           </h1>
-          <h5 className="block text-xl pt-4 font-medium text-inherit antialiased animate-text">
+          <h5 className="block text-2xl font-sans  pt-4 font-bold text-inherit antialiased animate-text">
             About:
           </h5>
           <p className="block text-md pt-4 font-normal text-inherit antialiased animate-text">
@@ -53,7 +65,7 @@ const Work = () => {
         </div>
 
         <div className="w-full pt-8 lg:pt-0 lg:pl-8 lg:w-1/2">
-          <h1 className="block text-2xl font-sans font-bold tracking-normal text-inherit antialiased">
+          <h1 className="block text-2xl font-sans font-bold tracking-normal animate-text text-inherit antialiased">
             Contributions :
           </h1>
           <ol className="p-8 list-disc">
@@ -65,7 +77,12 @@ const Work = () => {
           </ol>
         </div>
       </div>
-      <DurationSlider setXP={setXP} month={month} year={year} />
+      <DurationSlider 
+       setXP={setXP}
+       month={month} 
+       year={year} 
+       XP={XP} 
+      />
     </section>
   );
 };
